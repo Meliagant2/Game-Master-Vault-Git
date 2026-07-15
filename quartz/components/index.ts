a877f9e0ebef7eab9854bf599f792c7434cb5523
Meliagant2 <git@vhferdinand.com>
2026-07-15 15:42:40 +0200

    Update index.ts

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -6,6 +6,22 @@ import MobileOnly from "./MobileOnly"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
 
+/* Neu hinzugefügt */
+import MobileOnly from "./MobileOnly"
+import RecentNotes from "./RecentNotes"
+import Breadcrumbs from "./Breadcrumbs"
+import Properties from "./Properties"
+
+export {
+  ArticleTitle,
+@@ -42,4 +43,5 @@ export {
+  RecentNotes,
+  NotFound,
+  Breadcrumbs,
+  Properties
+}
+/* Neu Ende */
+
 export { componentRegistry, defineComponent } from "./registry"
 export { External } from "./external"
 export type { ComponentManifest, RegisteredComponent } from "./registry"
